189d663a7b7da2cf1c66f6f49c1f3982e3169c35
Mateo Bernasconi Vargas <bernasconivargas.mateo@gmail.com>
2024-03-06 18:57:31 -0600

    test trigger

--- test/unit/hello_test.js
+++ test/unit/hello_test.js
@@ -1,16 +1,13 @@
-'use strict';
+"use strict";
 
-const Code  = require('@hapi/code');
-const Lab   = require('@hapi/lab');
+const Code = require("@hapi/code");
+const Lab = require("@hapi/lab");
 
-const lab   = exports.lab = Lab.script();
-const Hello = require('../../lib/hello.js');
+const lab = (exports.lab = Lab.script());
+const Hello = require("../../lib/hello.js");
 
-lab.experiment('test greetings', () => {
-
-    lab.test('greets with name ', () => {
-
-        Code.expect(Hello('Homer')).to.equal('Hello, Homer!');
-    });
-
+lab.experiment("test greetings", () => {
+  lab.test("greets with name ", () => {
+    Code.expect(Hello("Homer")).to.equal("Hello, Homer!");
+  });
 });
